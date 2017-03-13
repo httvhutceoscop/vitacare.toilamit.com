95aa447f54c9baf306cf81ec1e35a9e2dc301cbc
PhantomLady <tienvietnguyen1110@gmail.com>
2017-03-14 00:02:28 +0700

    chane style and index

--- wp-content/themes/vitacare/index.php
+++ wp-content/themes/vitacare/index.php
@@ -26,6 +26,16 @@ echo do_shortcode('[smartslider3 slider=2]');
 <div class="container">
 	<div class="row">
 		 <div id="columns_left" class="col-xs-12 col-sm-12 col-md-12 col-lg-9">
+
+		 	<!-- Video giới thiệu -->
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<h3 class="title_home">Video giới thiệu Vitacare</h3>
+				<div class="video col-xs-12 col-sm-12 col-md-12 col-lg-12">
+					<iframe width="100%" height="auto" src="https://www.youtube.com/embed/BqRH8z3FAas" frameborder="0" allowfullscreen></iframe>	
+				</div>
+			</div>
+			<!-- End Video giới thiệu -->
+
 		 	<!-- Các sản phẩm vitacare -->
 			<div class="productbycategory col-xs-12 col-sm-12 col-md-12 col-lg-12">
 				<h3 class="title_home">Các sản phẩm VitaCare</h3>
@@ -128,7 +138,7 @@ echo do_shortcode('[smartslider3 slider=2]');
 
 			<!-- Kiến thức Vitacare -->
 
-			<div class="new-post-by-cat col-xs-12 col-sm-12 col-md-12 col-lg-12">
+			<div class="new-post-by-cat col-xs-12 col-sm-12 col-md-12 col-lg-12 hide">
 				<h3 class="title_home">Kiến thức Vitacare</h3>
 				<div class="column-new-post-by-cat col-xs-12 col-sm-6 col-md-4 col-lg-4">
 					<?php
@@ -225,23 +235,50 @@ echo do_shortcode('[smartslider3 slider=2]');
 							'post_type' => 'y-kien-khach-hang',
 							'showposts' => 3,
 							'orderby' => 'id',
-							'order' => 'ASC',
-							/*'tax_query' => array(
-											'taxonomy' => 'danh-muc-y-kien-khach-hang',
-										),*/
-							
+							'order' => 'ASC',							
 						);
-					query_posts($args);
-					if(have_posts()) : while(have_posts()) : the_post(); 
+					$aKhachHangReview = [];
+
+					$query_kh = new WP_Query($args);
+					$aYKienKhachHang = $query_kh->posts;
+
+					$video_ykkh = $aYKienKhachHang[0];
+					$image_ykkh = $aYKienKhachHang[1];
+					$text_ykkh = $aYKienKhachHang[2];
+			
+					$imgUrl = "https://img.youtube.com/vi/<insert-youtube-video-id-here>/default.jpg";
+					$youtubeId = get_post_meta($video_ykkh->ID, "youtube-id", $single = false);
 				?>
-					<ul class="com_guest col-xs-12 col-sm-12 col-md-4 col-lg-4">
-						<span class="com_content"><?php echo wp_trim_words(get_the_content(),70);?></span>
-						<a class="more-link" href="<?php the_permalink();?>">Đọc thêm...</a>
-					</ul>
+
 				<?php
-						endwhile;
-					endif;
+					if (!empty($youtubeId)) {
 				?>
+					<div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+						<div class="video">
+							<iframe width="100%" height="auto" src="https://www.youtube.com/embed/<?php echo $youtubeId[0];?>" frameborder="0" allowfullscreen></iframe>	
+						</div>
+					</div>
+				<?php		
+					} else {
+				?>
+
+				<?php } ?>
+
+				<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+						<a class="hide"> href="<?php echo get_permalink($image_ykkh->ID);?>">
+							<div class="img-ykkh">
+								<img src="https://placeholdit.imgix.net/~text?txtsize=19&txt=200%C3%97200&w=200&h=200"/>	
+							</div>
+						</a>
+						<?php echo wp_trim_words($image_ykkh->post_content,50);?>
+						<a class="more-link" href="<?php echo get_permalink($image_ykkh->ID);?>">Đọc thêm...</a>				
+					</div>
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+						<?php echo wp_trim_words($text_ykkh->post_content,50);?>
+						<a class="more-link" href="<?php echo get_permalink($text_ykkh->ID);?>">Đọc thêm...</a>
+					</div>
+				</div>
 			</div>
 			<!-- End: Khách hàng nói về Vitacare -->
 
